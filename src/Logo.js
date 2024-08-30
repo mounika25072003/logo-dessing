28dfb06dc1a677d1985b2d587923ba0fab12ca90
mounika25072003 <jarupatimounika@gmail.com>
2024-08-30 19:11:20 +0530

    ansss

---
+++ src/Logo.js
@@ -0,0 +1,18 @@
+// src/Logo.js
+import React from 'react';
+import './Logo.css';
+
+const Logo = () => {
+  return (
+    <div className="logo-container">
+        <div className='inner'>
+            <div className='text'>
+           <span className='html'> HTML <span  className='and'>&</span> CSS <br /></span>
+            <span className='span'>design and bulid Website</span>
+    </div>
+    </div>
+    </div>
+  );
+};
+
+export default Logo;
